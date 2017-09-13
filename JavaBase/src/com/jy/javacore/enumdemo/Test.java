68d6804a4e94b9725b4164b3f6f130fed2e51599
JAVAjy <1453813487@qq.com>
2017-09-13 17:46:38 +0800

    add to dongdong

--- JavaBase/src/com/jy/javacore/enumdemo/Test.java
+++ JavaBase/src/com/jy/javacore/enumdemo/Test.java
@@ -1,23 +1,78 @@
 package com.jy.javacore.enumdemo;
 
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map.Entry;
+@SuppressWarnings("all")
 public class Test {
 
 	public static void main(String[] args) {
 		// 遍历枚举所有的实例
-		System.out.println("输出WeekEnum:");
-		for(WeekEnum we :WeekEnum.values()) {
-			System.out.println(we);
-			System.out.println(we.name()+"----"+we.ordinal());
-			
-			
-			//遍历SeasonEnum
-			System.out.println("输出SeasonEnum:");
-			for(SeasonEnum se :SeasonEnum.values()) {
-				System.out.println(se);
-				se.printself();
-			}
+		// System.out.println("输出WeekEnum:");
+		// for (WeekEnum we : WeekEnum.values()) {
+		// System.out.println(we);
+		// System.out.println(we.name() + "----" + we.ordinal());
+		// }
+
+		// 遍历SeasonEnum
+		System.out.println("输出SeasonEnum:");
+		for (SeasonEnum se : SeasonEnum.values()) {
+			System.out.println(se);
+			se.printself();
+		}
+		// 和switch case 搭配使用
+		System.out.println("-----------");
+		SeasonEnum seasonEnum = SeasonEnum.SUMMER;
+		switch (seasonEnum) {
+		case SPRING:
+			System.out.println(seasonEnum);
+			break;
+		case SUMMER:
+			System.out.println(seasonEnum);
+			break;
+		case AUTUMN:
+			System.out.println(seasonEnum);
+			break;
+		case WINTER:
+			System.out.println(seasonEnum);
+			break;
+		default:
+			break;
+
+		}
+		// EnumSet demo
+		System.out.println("EnumSet:");
+		EnumSet<SeasonEnum> seasonSet = EnumSet.allOf(SeasonEnum.class);
+		for (SeasonEnum seasonEnum2 : seasonSet) {
+			System.out.println(seasonEnum2);
+		}
+
+		// EnumMap的使用
+		System.out.println("EnumMap:");
+		EnumMap<WeekEnum2, String> weekMap = new EnumMap(WeekEnum2.class);
+		weekMap.put(WeekEnum2.MON, "一");
+		weekMap.put(WeekEnum2.TUE, "二");
+		weekMap.put(WeekEnum2.WED, "三");
+		weekMap.put(WeekEnum2.THU, "四");
+		weekMap.put(WeekEnum2.FRI, "五");
+		weekMap.put(WeekEnum2.SAT, "六");
+		weekMap.put(WeekEnum2.SUN, "日");
+		for (Entry<WeekEnum2, String> entry : weekMap.entrySet()) {
+			System.out.println(entry.getKey().getIndex() + "--" + entry.getKey().getDesc() + "--" + entry.getValue());
+		}
+
+		// 枚举比较
+		WeekEnum2 weekEnum2 = WeekEnum2.MON;
+		int result = weekEnum2.compareTo(weekEnum2.SUN);
+		System.out.println("比较的结果：" + result);
+		if (result > 0) {
+			System.out.println("Mon在后面");
+		} else if (result == 0) {
+			System.out.println("Mon位置相同");
+		} else {
+			System.out.println("Mon位置前面");
+
 		}
 
 	}
-
 }
--- JavaBase/src/com/jy/javacore/enumdemo/WeekEnum2.java
+++ JavaBase/src/com/jy/javacore/enumdemo/WeekEnum2.java
@@ -1,19 +1,29 @@
 package com.jy.javacore.enumdemo;
 
+import lombok.Getter;
+
+@Getter
 public enum WeekEnum2 { // 使用enum 定义的枚举类默认继承了java.lang.Enum类
 	// 定义实例
-	MON, TUE, WES, THU, FRI, SAT {
+	MON(1, "星期一"), TUE(2, "星期二"), WED(3, "星期三"), THU(4, "星期四"), FRI(5, "星期五"), SAT(6, "星期六") {
 		// 提供判斷是否休息的方法
 		public boolean isRest() {
 			return true; // 默認休息
 		}
 	},
-	SUN {
+	SUN(7, "星期天") {
 		// 提供判斷是否休息的方法
 		public boolean isRest() {
 			return true; // 默認休息
 		}
 	};// 如果没有其他属性或者方法等，可以不写；，如果有一定要写；
+	private int index;
+	private String desc;
+
+	private WeekEnum2(int index, String desc) {
+		this.index = index;
+		this.desc = desc;
+	}
 
 	// 提供判斷是否休息的方法
 	public boolean isRest() {
