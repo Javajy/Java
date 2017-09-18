303d8337d1aa9195b8eca881bfefea3b6ceed3ae
JAVAjy <1453813487@qq.com>
2017-09-18 18:44:24 +0800

    add to jt2

---
+++ JavaBase/src/com/jy/javacore/javajt2oop/ColorDefine.java
@@ -0,0 +1,16 @@
+package com.jy.javacore.javajt2oop;
+
+public class ColorDefine {
+	String color = "黑色";
+
+	void getMes() {
+		System.out.println("定义类");
+	}
+
+	public static void main(String[] args) {
+		ColorDefine b = new ColorDefine();
+		System.out.println(b.color);
+		b.getMes();
+	}
+
+}
---
+++ JavaBase/src/com/jy/javacore/javajt2oop/Person.java
@@ -0,0 +1,11 @@
+package com.jy.javacore.javajt2oop;
+
+public class Person {
+
+	String name="王二小";
+	int age=5;
+
+	void talk() {
+		System.out.println("我是：" + name + ",今年" + age + "岁");
+	}
+}
